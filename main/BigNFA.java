bdcd2cf52105b7db2ae531fbd8f9ea7bd9268918
Ilyssa Widen <ilyssa@ilyssa-MacBook.(none)>
2013-04-11 01:10:25 -0400

    BigNFA

---
+++ main/BigNFA.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.*;
+
+public class BigNFA
+{
+	NFA fin;
+
+	public BigNFA(HashSet<NFA> NFATable)
+	{
+		fin=conversion(NFATable
+	}
+
+	public NFA conversion(HashSet<NFA> NFATAble)
+	{
+
+		return null;
+	}
+}
