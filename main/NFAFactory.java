4a7e98f47a7a2fb316cd5dfa622b7a5f1f3d21c8
Ilyssa Widen <ilyssa@ilyssa-MacBook.(none)>
2013-04-10 20:20:41 -0400

    make NFAFactory.java

---
+++ main/NFAFactory.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.*;
+
+public class NFAFactory
+{
+	HashMap<String,String> table;
+
+	//Will take in a table;		
+	public NFAFactory(HashMap<String,String> table)
+	{
+		this.table=table;	
+	}
+
+	public HashSet<NFA> factorize()
+	{
+		Set<String> keys=table.keySet();
+		for(String key: 	
+
+	}
+
+}
