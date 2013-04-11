2c00cc605b960bdbeb12144375a7973a7c6eb79b
Kurtis Eveleigh <kurtis.eveleigh@gmail.com>
2013-04-10 20:50:57 -0400

    NFAFactory additions.
    Yup.

--- main/NFAFactory.java
+++ main/NFAFactory.java
@@ -4,19 +4,24 @@ import java.util.*;
 
 public class NFAFactory
 {
-	HashMap<String,String> table;
+	HashMap<String, String> table;
 
 	//Will take in a table;		
-	public NFAFactory(HashMap<String,String> table)
+	public NFAFactory(HashMap<String, String> table)
 	{
-		this.table=table;	
+		this.table = table;	
 	}
 
 	public HashSet<NFA> factorize()
 	{
-		Set<String> keys=table.keySet();
-		for(String key: 	
-
+		HashSet<NFA> nfaSet = new HashSet<NFA>();
+		Set<String> keys = table.keySet();
+		for(String key : keys){
+			String value = table.get(key);
+			NFA create = new NFA(key, value);
+			nfaSet.add(create);
+		}
+		return nfaSet;
 	}
 
 }
