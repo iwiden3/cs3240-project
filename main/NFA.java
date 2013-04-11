95dc0181ced85ebddca99ef732dab2a9185efeb2
Paul Dewey <pdewey17@gmail.com>
2013-04-10 20:46:16 -0400

    Adds logic to NFA.java

--- main/NFA.java
+++ main/NFA.java
@@ -4,16 +4,39 @@ import java.util.*;
 
 public class NFA 
 {
-	State start;
-	String name;
+	private State start;
+	private String name;
 
-	public NFA()
+	public NFA(String name, String regex)
 	{
-		this.start=start;
+        this.name = name;
+
+        // Make accept state
+        // TODO: this is for simple char classes only
+        State accept = new State(true, new HashMap<String, State>());
+        
+        // Create transition table
+        HashMap<String, State> transition = new HashMap<String, State>();
+        transition.add(regex, accept);
+        
+        // Create start state
+        if (!regex.equals(""))
+        {
+            start = new State(false, transition);
+        }
+        else
+        {
+            start = new State(true, transition);
+        }
 	}
 
-	
+    public String getName()
+    {
+        return name;
+    }
 
-
-
+    public State getStart()
+    {
+        return start;
+    }
 }
