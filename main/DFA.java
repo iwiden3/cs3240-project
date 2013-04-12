ac5aef34cfad053ece34e3c78581bd9ad97d876b
Paul Dewey <pdewey17@gmail.com>
2013-04-11 21:55:27 -0400

    Adds first sets

--- main/DFA.java
+++ main/DFA.java
@@ -23,22 +23,27 @@ public class DFA
     private HashSet<State> constructEClosure(State s)
     {
         HashSet<State> eStates = new HashSet<State>();
+        HashSet<State> visited = new HashSet<State>();
         HashMap<String, List<State>> transTable = s.getTransitionTable();
 
         // Adds the original state to the set
         eStates.add(s);
+        visited.add(s);
 
         // Iterate over all epsilon transitions and add them to the set
         for (State st : transTable.get(""))
         {
             eStates.add(st);
 
-            // Add all epsilon transitions from these episilon transitions
-            // Might have to consider endless epsilon looping
-            for (State e : constructEClosure(st))
+            // Add all epsilon transitions from the new states
+            if (!visited.contains(st))
             {
-                eStates.add(e);
+                for (State e : constructEClosure(st))
+                {
+                    eStates.add(e);
+                }
             }
+            visited.add(st);
         }
 
         return eStates;
